700299ea613f3012e3226bcf8c7a58cf12240e0c
NioParabellum <128557296+Weenio@users.noreply.github.com>
2024-04-30 20:33:43 -0300

    commit inicial

---
+++ CREATE-DATABASE-COLLECTION.mongodb.js
@@ -0,0 +1,15 @@
+/*Define o nome do banco de dados*/
+
+const database = 'libri-database'
+
+/*Define o nome da collection*/
+
+const collection = 'libri-collection'
+
+/*Acessa o banco de dados*/
+
+use(database)
+
+/*Cria a collection*/
+
+db.createCollection(collection)
---
+++ INSERT-DATA.mongodb.js
@@ -0,0 +1,11 @@
+/*Define o nome do banco de dados*/
+
+const database = 'libri-database'
+
+/*Define o nome da collection*/
+
+const collection = 'libri-collection'
+
+/*Acessa o banco de dados*/
+
+use(database)
